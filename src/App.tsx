af2bc758b51a09baf49f7ed373c45d3aa558b4bf
Dalboviken Holding <dalboviken.holding@gmail.com>
2026-06-15 15:22:22 +0200

    remove: clouds component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import Navbar from './components/Navbar'
 import HeroBackground from './components/HeroBackground'
-import Clouds from './components/Clouds'
 import HeroContent from './components/HeroContent'
 import SponsorLogos from './components/SponsorLogos'
 import BorderFrame from './components/BorderFrame'
@@ -10,7 +9,6 @@ export default function App() {
   return (
     <div className="relative w-full h-screen overflow-hidden" style={{ background: '#080d18' }}>
       <HeroBackground />
-      <Clouds />
       <FilmGrain />
       <BorderFrame />
       <Navbar />
